1acd80d533f95774360ab6a6b0395c62bf09fcee
Aditya Patil <outofaditya@gmail.com>
2026-04-20 14:18:07 +0200

    Remove element-ui alias from vite config - use built package

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@v-ems/element": path.resolve(__dirname, "../element-ui/src"),
     },
   },
   server: {
