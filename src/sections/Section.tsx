05c95a286da68a51ab0ea4a809cf4924c84ba57c
Aditya Patil <outofaditya@gmail.com>
2026-04-20 16:02:47 +0200

    fix: section component

--- src/sections/Section.tsx
+++ src/sections/Section.tsx
@@ -38,7 +38,7 @@ export function Section({
     <section id={id} className={sectionClassName}>
       <div className={containerClassNameMerged}>
         <Grid className="absolute inset-0 z-0" />
-        <h2 className="relative z-10 mt-0 w-full bg-primary text-3xl font-bold tracking-tight text-foreground sm:text-4xl lg:text-5xl">
+        <h2 className="relative z-10 mt-0 w-full text-3xl font-bold tracking-tight text-foreground sm:text-4xl lg:text-5xl">
           {title}
         </h2>
       </div>
